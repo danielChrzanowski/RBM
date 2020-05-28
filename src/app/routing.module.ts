d366232880f009300eb13150fdae55e89a70726f
Daniel <daniel_chrzanowski98@wp.pl>
2020-05-28 13:28:57 +0200

    fixed routing module

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,29 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { HomeComponent } from './pages/home/home.component';
-
-import { ShowOrdersComponent } from './pages/show-orders/show-orders.component';
-import { CreateUserComponent } from './pages/create-user/create-user.component';
-import { LogInComponent } from './pages/log-in/log-in.component';
-import { AuthGuard } from './authentication/auth-guard-client/auth.guard';
-import { ClientOrdersComponent } from './pages/client-orders/client-orders.component';
+import { RouterModule, Routes } from '@angular/router';
 import { ClientMakeOrderComponent } from './pages/client-make-order/client-make-order.component';
+import { ClientOrdersComponent } from './pages/client-orders/client-orders.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 import { RestauramtMenuComponent } from './pages/restauramt-menu/restauramt-menu.component';
-import { AuthGuardEmployee } from './authentication/auth-guard-employee/auth-guard-employee';
+import { ShowOrdersComponent } from './pages/show-orders/show-orders.component';
 import { UserInfoComponent } from './pages/user-info/user-info.component';
-import { UserAuthGuard } from './authentication/user-auth-guard/user-auth-guard';
+import { ClientAuthGuard } from './_authentication/client-auth-guard/client-auth.guard';
+import { EmployeeAuthGuard } from './_authentication/employee-auth-guard/employee-auth-guard';
+import { UserAuthGuardService } from './_authentication/user-auth-guard/user-auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'log-in', component: LogInComponent },
-  { path: 'showOrders', canActivate: [AuthGuardEmployee], component: ShowOrdersComponent },
+  { path: 'showOrders', canActivate: [EmployeeAuthGuard], component: ShowOrdersComponent },
   { path: 'createUser', component: CreateUserComponent },
   { path: 'menu', component: RestauramtMenuComponent },
-  { path: 'clientOrders', canActivate: [AuthGuard], component: ClientOrdersComponent },
-  { path: 'makeOrder', canActivate: [AuthGuard], component: ClientMakeOrderComponent },
-  { path: 'userInfo', canActivate: [UserAuthGuard], component: UserInfoComponent },
+  { path: 'clientOrders', canActivate: [ClientAuthGuard], component: ClientOrdersComponent },
+  { path: 'makeOrder', canActivate: [ClientAuthGuard], component: ClientMakeOrderComponent },
+  { path: 'userInfo', canActivate: [UserAuthGuardService], component: UserInfoComponent },
 ];
 
 @NgModule({
